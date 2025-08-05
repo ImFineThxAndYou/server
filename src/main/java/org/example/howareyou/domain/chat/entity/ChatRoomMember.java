e25f7fd94f57dafe15269d9e4742a0aecfd90090
sagecode <iamsage9346@gmail.com>
2025-08-05 09:57:41 +0900

    feat/#5 - ChatRoom 관련 Entity

---
+++ src/main/java/org/example/howareyou/domain/chat/entity/ChatRoomMember.java
@@ -0,0 +1,39 @@
+package org.example.howareyou.domain.chat.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.howareyou.domain.member.entity.Member;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChatRoomMember {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "chat_room_id")
+  private ChatRoom chatRoom;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Enumerated(EnumType.STRING)
+  private ChatRoomMemberStatus status;
+
+  @Column(updatable = false)
+  private LocalDateTime joinedAt;
+
+  public ChatRoomMember(ChatRoom chatRoom, Member member, ChatRoomMemberStatus status) {
+    this.chatRoom = chatRoom;
+    this.member = member;
+    this.status = status;
+  }
+}
