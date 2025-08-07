ebf3116b473c85cb6e5b0c233932da793105cb21
sagecode <iamsage9346@gmail.com>
2025-08-07 11:18:23 +0900

    feat/#15 - controller

--- src/main/java/org/example/howareyou/domain/chat/websocket/controller/ChatMessageController.java
+++ src/main/java/org/example/howareyou/domain/chat/websocket/controller/ChatMessageController.java
@@ -1,7 +1,11 @@
 package org.example.howareyou.domain.chat.websocket.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.howareyou.domain.chat.websocket.dto.ChatEnterDTO;
 import org.example.howareyou.domain.chat.websocket.entity.ChatMessageDocument;
+import org.example.howareyou.domain.chat.websocket.service.ChatMessageService;
+import org.example.howareyou.domain.chat.websocket.service.ChatRedisService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -9,16 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ChatMessageController {
 
   private final SimpMessagingTemplate messagingTemplate;
+  private final ChatMessageService chatMessageService;
+  private final ChatRedisService chatRedisService;
 
   // 메시지 전송 엔드포인트: /app/chat.send
   @MessageMapping("/chat.send")
   public void sendMessage(@Payload ChatMessageDocument chatMessageDocument) {
 
+    chatMessageService.saveChatMessage(chatMessageDocument); // 저장
     messagingTemplate.convertAndSend(
         "/topic/chatroom/" + chatMessageDocument.getChatRoomUuid(), chatMessageDocument
     );
   }
+
+  @MessageMapping("/chat.enter")
+  public void enterRoom(ChatEnterDTO dto) {
+    String userId = dto.getUserId();
+    String chatRoomId = dto.getChatRoomId();
+
+    chatRedisService.setCurrentChatRoom(userId, chatRoomId);
+    chatMessageService.markMessagesAsRead(chatRoomId, userId);
+
+    log.info("유저 입장: userId={}, chatRoomId={}", userId, chatRoomId);
+  }
+
 }
