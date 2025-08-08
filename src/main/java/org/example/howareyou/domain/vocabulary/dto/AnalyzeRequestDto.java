a1653254f071bb9ddfd22c104107937afd20f275
AYoung Eom <eomssi99@gmail.com>
2025-08-08 15:19:45 +0900

    feat/#30 - 채팅방 별 단어장 조회

--- src/main/java/org/example/howareyou/domain/vocabulary/dto/AnalyzeRequestDto.java
+++ src/main/java/org/example/howareyou/domain/vocabulary/dto/AnalyzeRequestDto.java
@@ -2,9 +2,11 @@ package org.example.howareyou.domain.vocabulary.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class AnalyzeRequestDto {
     private String text;
 }
--- src/main/java/org/example/howareyou/domain/vocabulary/repository/ChatRoomVocabularyRepository.java
+++ src/main/java/org/example/howareyou/domain/vocabulary/repository/ChatRoomVocabularyRepository.java
@@ -4,5 +4,12 @@ package org.example.howareyou.domain.vocabulary.repository;
 import org.example.howareyou.domain.vocabulary.document.ChatRoomVocabulary;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ChatRoomVocabularyRepository extends MongoRepository<ChatRoomVocabulary, String> {
+    List<ChatRoomVocabulary> findByChatRoomUuidOrderByAnalyzedAtDesc(String chatRoomUuid);
+
+    List<ChatRoomVocabulary> findAllByOrderByAnalyzedAtDesc();
+
 }
