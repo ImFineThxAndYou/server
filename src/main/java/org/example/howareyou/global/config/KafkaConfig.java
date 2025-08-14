81d4f58101a53e361f1dc5f9f1d4f71be2fb317f
sung_1 <moonjs1011@naver.com>
2025-08-14 10:24:21 +0900

    Update : Kafka 연결 설정 파일 추가 [#49]

---
+++ src/main/java/org/example/howareyou/global/config/KafkaConfig.java
@@ -0,0 +1,32 @@
+package org.example.howareyou.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> props = kafkaProperties.buildProducerProperties();
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
--- src/main/java/org/example/howareyou/global/test/TestProducer.java
+++ src/main/java/org/example/howareyou/global/test/TestProducer.java
@@ -1,12 +1,15 @@
 package org.example.howareyou.global.test;
 
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TestProducer {
-    private final KafkaTemplate<String, Object> kafkaTemplate;
-    public  TestProducer(KafkaTemplate<String, Object> kafkaTemplate) {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate
+;    public  TestProducer(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
     public void create(){
